d06f92fa1eced3b0436d5bc46f87682c903d5807
Thomas <thomasmaffezzolli8@gmail.com>
2022-06-27 22:43:43 -0300

    criacao da barra de progessao

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+let processScroll = () => {
+    let docElem = document.documentElement, 
+        docBody = document.body,
+        scrollTop = docElem['scrollTop'] || docBody['scrollTop'],
+        scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight'] - window.innerHeight),
+        scrollPercent = scrollTop / scrollBottom * 100 + '%';
+
+    document.getElementById('progress-bar').style.setProperty('width', scrollPercent);
+    console.log(scrollPercent);
+}
+
+document.addEventListener('scroll', processScroll);
